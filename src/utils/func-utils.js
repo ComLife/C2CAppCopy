ee700172b8a650bdb48c31aef88603ab6a98e60a
汪春广 <1070034079@qq.com>
2019-10-14 20:05:04 +0800

    添加常用函数方法

--- src/utils/func-utils.js
+++ src/utils/func-utils.js
@@ -57,3 +57,69 @@ export const isObjectValueEqual = (obj1: any, obj2: any) => {
   }
   return true;
 };
+
+/**
+ * 判断是否为数字类型
+ */
+export const isDigit = value => {
+  let patrn = /^[0-9]*$/;
+  if (patrn.exec(value) == null || value === '') {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+/**
+ * 千分位
+ */
+export const Convert = money => {
+  let s = money; //获取小数型数据
+  s += '';
+  if (s.indexOf('.') === -1) {
+    s += '.00';
+  } //如果没有小数点，在后面补个小数点和00
+  if (/\.\d$/.test(s)) {
+    s += '0';
+  } //正则判断
+  while (
+    /\d{4}(\.|,)/.test(s) //符合条件则进行替换
+  ) {
+    s = s.replace(/(\d)(\d{3}(\.|,))/, '$1,$2');
+  } //每隔3位添加一个
+  return s;
+};
+
+// 定义一个深拷贝函数  接收目标target参数
+export const deepClone = target => {
+  // 定义一个变量
+  let result;
+  // 如果当前需要深拷贝的是一个对象的话
+  if (typeof target === 'object') {
+    // 如果是一个数组的话
+    if (Array.isArray(target)) {
+      result = []; // 将result赋值为一个数组，并且执行遍历
+      for (let i in target) {
+        // 递归克隆数组中的每一项
+        result.push(deepClone(target[i]));
+      }
+      // 判断如果当前的值是null的话；直接赋值为null
+    } else if (target === null) {
+      result = null;
+      // 判断如果当前的值是一个RegExp对象的话，直接赋值
+    } else if (target.constructor === RegExp) {
+      result = target;
+    } else {
+      // 否则是普通对象，直接for in循环，递归赋值对象的所有值
+      result = {};
+      for (let i in target) {
+        result[i] = deepClone(target[i]);
+      }
+    }
+    // 如果不是对象的话，就是基本数据类型，那么直接赋值
+  } else {
+    result = target;
+  }
+  // 返回最终结果
+  return result;
+};
